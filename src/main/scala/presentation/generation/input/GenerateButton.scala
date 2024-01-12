e0dba759233a77f66b09227ae21da45ec93fd7fc
dinaraparanid <dinaraparanid@gmail.com>
2024-01-12 13:37:13 +0300

    table packing

--- src/main/scala/presentation/generation/input/GenerateButton.scala
+++ src/main/scala/presentation/generation/input/GenerateButton.scala
@@ -1,15 +1,18 @@
 package presentation.generation.input
 
 import presentation.sessionStates
-import presentation.ui.utils.{removeActionListeners, combine}
+import presentation.ui.utils.{combine, removeActionListeners}
+
 import data.app.{SessionStates, isInputCorrectStream}
 import data.generation.population.TableState
+
 import domain.generation.generation
 import domain.session.parsedWordsWithMeanings
+import domain.session.packing.packed
 
 import cats.implicits.*
 
-import zio.{URIO, ZIO}
+import zio.{URIO, UIO, ZIO, Runtime, Unsafe}
 
 import javax.swing.JButton
 
@@ -17,12 +20,17 @@ def GenerateButton(): URIO[SessionStates, JButton] =
   val button = new JButton("Generate"):
     putClientProperty("JButton.buttonType", "roundRect")
 
+  val runtime = Runtime.default
+
   def recompose(isCrosswordCorrect: Boolean, wordsInput: String): Unit =
     button setEnabled isCrosswordCorrect
     button.removeActionListeners()
     button addActionListener: _ ⇒
-      val table = generateCrossword(wordsInput)
-      println(table.show)
+      Unsafe.unsafe { implicit unsafe ⇒
+        runtime.unsafe.runToFuture:
+          for table ← generateCrossword(wordsInput)
+            yield println(table.show)
+      }
 
   for {
     inputs            ← sessionStates()
@@ -36,11 +44,11 @@ def GenerateButton(): URIO[SessionStates, JButton] =
       .fork
   } yield button
 
-private def generateCrossword(wordsInput: String): TableState =
+private def generateCrossword(wordsInput: String): UIO[TableState] =
   val wordsWithMeanings = parsedWordsWithMeanings(wordsInput)
   val words = wordsWithMeanings.keys.toList
   val tabSize = requiredTableSize(words)
-  generation(words, tabSize)
+  packed(generation(words, tabSize))
 
 private def requiredTableSize(words: Iterable[String]): Int =
   val maxLength = words.map(_.length).max
